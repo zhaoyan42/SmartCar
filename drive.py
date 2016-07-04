e34e484a3a05f2c4779e6c09cc00363395321d3f
zhaoyan42 <kitten63928@live.com>
2016-07-04 23:20:21 +0800

    Add L298N Module Model Class

--- drive.py
+++ drive.py
@@ -1,42 +1,62 @@
 import RPi.GPIO as GPIO
 import time
 
+class L298N_GPIO(object):
+    '''
+    This Class is for GPIO to control L298N
+    '''
+
+    __in1 = 0
+    __in2 = 0
+    __in3 = 0
+    __in4 = 0
+    __real_true = GPIO.HIGH
+
+    def __init__(self,in1,in2,in3,in4,real_true = GPIO.HIGH):
+        self.__in1 = in1
+        GPIO.setup(self.__in1,GPIO.OUT)
+        self.__in2 = in2
+        GPIO.setup(self.__in2,GPIO.OUT)
+        self.__in3 = in3
+        GPIO.setup(self.__in3,GPIO.OUT)
+        self.__in4 = in4
+        GPIO.setup(self.__in4,GPIO.OUT)
+        self.__real_true = real_true
+
+    def forward(self):
+        GPIO.output(self.__in1,self.__real_true)
+        GPIO.output(self.__in2,not self.__real_true)
+        GPIO.output(self.__in3,not self.__real_true)
+        GPIO.output(self.__in4,self.__real_true)
+
+    def back(self):
+        GPIO.output(self.__in1,not self.__real_true)
+        GPIO.output(self.__in2,self.__real_true)
+        GPIO.output(self.__in3,self.__real_true)
+        GPIO.output(self.__in4,not self.__real_true)
+
 GPIO.cleanup()
 
-in1 = 18
-in2 = 23
-in3 = 24
-in4 = 25
-
-in5 = 12
-in6 = 16
-in7 = 20
-in8 = 21
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
 
-GPIO.setup(in5,GPIO.OUT)
-GPIO.setup(in6,GPIO.OUT)
-GPIO.setup(in7,GPIO.OUT)
-GPIO.setup(in8,GPIO.OUT)
+l298n1 = L298N_GPIO(18,23,24,25,GPIO.HIGH)
+l298n2 = L298N_GPIO(12,16,20,21,GPIO.LOW)
 
 
+l298n1.forward()
+l298n2.forward()
+time.sleep(3)
 
+l298n1.back()
+l298n2.back()
+time.sleep(3)
 
-GPIO.output(in1,True)
-GPIO.output(in2,False)
-GPIO.output(in3,False)
-GPIO.output(in4,True)
+l298n1.forward()
+l298n2.back()
+time.sleep(3)
 
-GPIO.output(in5,False)
-GPIO.output(in6,True)
-GPIO.output(in7,True)
-GPIO.output(in8,False)
-
+l298n1.back()
+l298n2.forward()
 time.sleep(3)
 
 GPIO.cleanup()
