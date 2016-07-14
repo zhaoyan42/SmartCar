ff1d750037c4de6d378868268a30938dc3259177
赵晏龙 <kitten63928@live.com>
2016-07-14 23:57:47 +0800

    Add Class Wheel

--- Arduino/SmartCar/SmartCar.ino
+++ Arduino/SmartCar/SmartCar.ino
@@ -5,19 +5,25 @@
 */
 
 #include "Wheel.h"
-int in1_pin, in2_pin;
+
+Wheel wheel1(5, 2, 4);
+Wheel wheel2(6, 7, 8);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	in1_pin = 5;
-	in2_pin = 6;
-	pinMode(in1_pin, OUTPUT);
-	pinMode(in2_pin, OUTPUT);
-	digitalWrite(in1_pin, HIGH);
-	digitalWrite(in2_pin, LOW);
+
+	wheel1.lunch();
+	wheel2.lunch();
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
 
+	wheel1.run();
+	wheel2.run();
+	delay(2000);
+	wheel1.stop();
+	wheel2.stop();
+	delay(1000);
 }
--- Arduino/SmartCar/Wheel.h
+++ Arduino/SmartCar/Wheel.h
@@ -4,21 +4,45 @@
 #define _WHEEL_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
-class WheelClass
+class Wheel
 {
- protected:
+private:
+	int enable_pin, in1_pin, in2_pin;
 
+protected:
 
- public:
-	void init();
+public:
+	Wheel(int enable_pin, int in1_pin, int in2_pin)
+	{
+		this->enable_pin = enable_pin;
+		this->in1_pin = in1_pin;
+		this->in2_pin = in2_pin;
+	}
+
+	void lunch()
+	{
+		pinMode(enable_pin, OUTPUT);
+		pinMode(in1_pin, OUTPUT);
+		pinMode(in2_pin, OUTPUT);
+	}
+
+	void run()
+	{
+		analogWrite(enable_pin, 255);
+		digitalWrite(in1_pin, HIGH);
+		digitalWrite(in2_pin, LOW);
+	}
+
+	void stop()
+	{
+		analogWrite(enable_pin, 0);
+	}
 };
-
-extern WheelClass Wheel;
 
 #endif
 
