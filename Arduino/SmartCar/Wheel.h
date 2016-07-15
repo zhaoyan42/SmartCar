164f0a9a900417ce47691403c86885d4bfec2adf
赵晏龙 <kitten63928@live.com>
2016-07-16 01:21:13 +0800

    split speed and wheel drection to stand alone function to control wheel unitization

--- Arduino/SmartCar/Wheel.h
+++ Arduino/SmartCar/Wheel.h
@@ -9,6 +9,7 @@
 #include "WProgram.h"
 #endif
 
+
 class Wheel
 {
 private:
@@ -17,11 +18,30 @@ private:
 protected:
 
 public:
+	
 	Wheel(int enable_pin, int in1_pin, int in2_pin)
 	{
 		this->enable_pin = enable_pin;
 		this->in1_pin = in1_pin;
 		this->in2_pin = in2_pin;
+	}
+	
+	enum WheelDirection
+	{
+		Positive,
+		Negative
+	};
+
+	String get_wheel_direction_name(WheelDirection wheel_direction)
+	{
+		switch (wheel_direction)
+		{
+		case Positive:
+			return "Positive";
+
+		case Negative:
+			return "Negative";
+		}
 	}
 
 	void lunch()
@@ -31,16 +51,33 @@ public:
 		pinMode(in2_pin, OUTPUT);
 	}
 
-	void run(int speed=100)
+	void speed(int speed = 100)
 	{
-		analogWrite(enable_pin, map(speed,0,100,0,255));
-		digitalWrite(in1_pin, HIGH);
-		digitalWrite(in2_pin, LOW);
+		analogWrite(enable_pin, map(speed, 0, 100, 0, 255));
+	}
+
+	void rotate_direction(WheelDirection wheel_direction)
+	{
+		switch (wheel_direction)
+		{
+		case Positive:
+			digitalWrite(in1_pin, HIGH);
+			digitalWrite(in2_pin, LOW);
+			break;
+
+		case Negative:
+			digitalWrite(in1_pin, LOW);
+			digitalWrite(in2_pin, HIGH);
+			break;
+
+		default:
+			throw "Unsupported Direction" + get_wheel_direction_name(wheel_direction);
+		}
 	}
 
 	void stop()
 	{
-		analogWrite(enable_pin, 0);
+		speed(0);
 	}
 };
 
