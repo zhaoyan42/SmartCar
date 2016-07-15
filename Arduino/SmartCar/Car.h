591388d0a61f1a9cf6ee324dfb2637335e8a63ae
赵晏龙 <kitten63928@live.com>
2016-07-16 00:13:27 +0800

    add destructor for Car class

--- Arduino/SmartCar/Car.h
+++ Arduino/SmartCar/Car.h
@@ -12,20 +12,28 @@
 
 class Car
 {
+private:
+	Car(Wheel* wheel1, Wheel* wheel2)
+		: wheel1(wheel1),
+		wheel2(wheel2)
+	{
+	}
+
+	~Car()
+	{
+		delete this->wheel1;
+		delete this->wheel2;
+	}
+
 protected:
 	Wheel *wheel1, *wheel2;
-
+	
 public:
 	Car(int en1, int in1_1, int in1_2, int en2, int in2_1, int in2_2)
 		:Car(new Wheel(en1, in1_1, in1_2), new Wheel(en2, in2_1, in2_2))
 	{
 	}
 
-	Car(Wheel* wheel1, Wheel* wheel2)
-		: wheel1(wheel1),
-		wheel2(wheel2)
-	{
-	}
 
 	void lunch()
 	{
