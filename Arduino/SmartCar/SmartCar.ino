6e248ffd57935415a8476d3ce1c02a012551d935
赵晏龙 <kitten63928@live.com>
2016-07-25 00:13:17 +0800

    imporve avoid algorithm to get car run smooth

--- Arduino/SmartCar/SmartCar.ino
+++ Arduino/SmartCar/SmartCar.ino
@@ -13,6 +13,12 @@ Car car(6, 7, 8, 5, 9, 4);
 DistanceDetector distance_detector_high(A0, A1);
 DistanceDetector distance_detector_low(A2, A3);
 Servo servo;
+const auto servo_right_anlge = 45;
+const auto servo_middle_anlge = 90;
+const auto servo_left_anlge = 135;
+auto is_turning = false;
+float right_distance = 0;
+float left_distance = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -27,6 +33,7 @@ void setup() {
 	distance_detector_low.lunch();
 
 	servo.attach(A5);
+	servo.write(servo_middle_anlge);
 
 }
 
@@ -39,7 +46,7 @@ float get_distance()
 	return distance / 3.0;
 };
 
-long limited_map(long soruce,long soruce_min,long soruce_max,long target_min,long target_max)
+long limited_map(long soruce, long soruce_min, long soruce_max, long target_min, long target_max)
 {
 	if (soruce < soruce_min)soruce = soruce_min;
 	if (soruce > soruce_max)soruce = soruce_max;
@@ -48,37 +55,40 @@ long limited_map(long soruce,long soruce_min,long soruce_max,long target_min,lon
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	servo.write(90);
-	auto middle_high = distance_detector_high.get_distance();
-	auto middle_low = distance_detector_low.get_distance();
-	auto speed = limited_map((middle_low+ middle_high)/2, 30, 100, 50, 100);
-	Serial.println(speed);
-	if (middle_high < 40 || middle_low <40)
+	auto distance_high = distance_detector_high.get_distance();
+	auto distance_low = distance_detector_low.get_distance();
+	auto speed = limited_map((distance_low + distance_high) / 2, 30, 100, 50, 100);
+	bool too_close = distance_high < 40 || distance_low < 40;
+	if (!is_turning && too_close)
 	{
-		car.stop();
-
-		servo.write(45);
+		servo.write(servo_right_anlge);
 		delay(200);
-		float right_distance = get_distance();
+		right_distance = get_distance();
 
-		servo.write(135);
+		servo.write(servo_left_anlge);
 		delay(200);
-		float left_distance = get_distance();
-		
-		servo.write(90);
+		left_distance = get_distance();
 
-		if(right_distance>left_distance)
+		is_turning = true;
+	}
+	if (is_turning) {
+		if (!too_close)is_turning = false;
+		Serial.print(right_distance);
+		Serial.print(" ");
+		Serial.println(left_distance);
+		if (right_distance > left_distance)
 		{
+			servo.write(servo_left_anlge);
 			car.turn_right(speed);
+			return;
 		}
 		else
 		{
+			servo.write(servo_right_anlge);
 			car.turn_left(speed);
+			return;
 		}
-		delay(500);
 	}
-	else
-	{
-		car.forward(speed);
-	}
+	servo.write(servo_middle_anlge);
+	car.forward(speed);
 }
